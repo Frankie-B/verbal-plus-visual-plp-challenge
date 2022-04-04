df5e57bbd173ddeb50ffd02d026ea1583194582f
Frankie Bukenya <frankie.bukenya@gmail.com>
2022-04-04 20:44:00 +0200

    +(feat): Get products from storfront API

--- src/assets/scripts/templates/collection.js
+++ src/assets/scripts/templates/collection.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import getAllProducts from '../graphql/collection-starter-code';
 import '../theme';
 
 
 const Collection = () => {
 
-  // TODO: Get products from getAllProducts funciton
-  console.log(getAllProducts);
+  /** Since I am trying to retrieve data from an asynchronous function, using the componentDidMount hook would
+   * create an infinite render loop when the function is call. Instead I am choosing to use the useEffect hook, to only
+   * re-render if specific data changes.
+   */
+
+  const [products, setProducts] = useState();
+
+   useEffect(() => {
+    getAllProducts('test-collection')
+      .then(data => {
+        setProducts(data)
+      }).catch(err => {
+         console.log(err)
+       })
+  }, [])
+
+  console.log(products)
 
   return (
-    <div className="Collection">This is a test!!</div>
+    <div className="Collection">Products will go here</div>
   )
 }
 
