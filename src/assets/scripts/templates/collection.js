1be30e125f89ce677f90eab7c93b3b1b9966e0aa
Frankie Bukenya <frankie.bukenya@gmail.com>
2022-04-06 22:37:59 +0200

    +(feat): Show t-shirts

--- src/assets/scripts/templates/collection.js
+++ src/assets/scripts/templates/collection.js
@@ -2,30 +2,55 @@ import React, { useEffect, useState } from "react";
 import getAllProducts from '../graphql/collection-starter-code';
 import '../theme';
 
-
 const Collection = () => {
+  const [products, setProducts] = useState([]);
+  /* Resolve the getAllProducts function in a hook, useEffect is good for this, to avoid a scenario of the page infinitely
+  rendering/calling the function */
+  useEffect(() => {
+    let mounted = true
 
-  /** Since I am trying to retrieve data from an asynchronous function, using the componentDidMount hook would
-   * create an infinite render loop when the function is call. Instead I am choosing to use the useEffect hook, to only
-   * re-render if specific data changes.
-   */
-
-  const [products, setProducts] = useState();
-
-   useEffect(() => {
-    getAllProducts('test-collection')
-      .then(data => {
-        setProducts(data)
-      }).catch(err => {
-         console.log(err)
-       })
+    /** Async function to retrieve the data from the getAllProducts Fn and return its value */
+   async function fetchProducts() {
+     try {
+       const result = await getAllProducts('test-collection');
+       if (mounted) setProducts(result);
+     } catch (error) {
+       console.error(`Something went wrong: ${err}`) // please, please, properly handle this error in React :)
+     }
+   }
+   fetchProducts();
+   return () => {
+     mounted = false;
+   }
   }, [])
 
-  console.log(products)
+
+  const collection = products
+    .filter(product => product.handle === 'short-sleeve-t-shirt') // filter for only the t-shirts
+    .flatMap(product => product.variants.map(variant => [variant, variant.image.src, variant.price])); // flatten to one level
+
+  console.log(collection, typeof (collection));
+
+  const productCard = collection.map((product, index) => {
+    const imageUrl = product[1];
+    // const price = product[2].toFixed(2);
+    return (
+      <div className="product_container" key={index}>
+        <div className="image_container">
+           <img src={imageUrl} alt="" />
+        </div>
+        <div className="details_container">
+
+        </div>
+      </div>
+    )
+   } )
 
   return (
-    <div className="Collection">Products will go here</div>
-  )
+    <div className="Collection">
+      {productCard}
+    </div>
+  );
 }
 
 export default Collection;
