387ab3aae2504faa2117dfa44f7020fee025c94f
Frankie Bukenya <frankie.bukenya@mediamonks.com>
2022-04-09 02:03:22 +0200

    =(feat): Add all product cards

--- src/assets/scripts/templates/collection.js
+++ src/assets/scripts/templates/collection.js
@@ -1,85 +1,100 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import '../../styles/templates/collection.scss';
 import getAllProducts from '../graphql/collection-starter-code';
 import '../theme';
+import {unique} from "webpack-merge";
 
 const Collection = () => {
-  const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
-  /* Resolve the getAllProducts function in a hook, useEffect is good for this, to avoid a scenario of the page infinitely
-  rendering/calling the function */
-  useEffect(() => {
-    let mounted = true
+    /* Resolve the getAllProducts function in a hook, useEffect is good for this, to avoid a scenario of the page infinitely
+    rendering/calling the function */
+    useEffect(() => {
+        let mounted = true
 
-    /** Async function to retrieve the data from the getAllProducts Fn and return its value */
-   async function fetchProducts() {
-     try {
-       const result = await getAllProducts('test-collection');
-       if (mounted) setProducts(result);
-     } catch (error) {
-       console.error(`Something went wrong: ${err}`) // please, please, properly handle this error in React :)
-     }
-   }
+        /** Async function to retrieve the data from the getAllProducts Fn and return its value */
+        async function fetchProducts() {
+            try {
+                const result = await getAllProducts('test-collection');
+                if (mounted) setProducts(result);
+            } catch (error) {
+                console.error(`Something went wrong: ${err}`) // please, please, properly handle this error in React :)
+            }
+        }
 
+        fetchProducts();
 
-  fetchProducts();
+        return () => {
+            mounted = false;
+        }
+    }, [])
 
-   return () => {
-     mounted = false;
-   }
-  }, [])
+    // console.log(products);
 
+    // create an array of all the prod
+    // const collection = products
+    //     .filter(product => product.title !== null) // filter for only the t-shirts
+    //     .flatMap(product => product.variants.map(variant => [variant, variant.image.src, variant.price]));
 
-  // create an array of all the t-shirts
-  const collection = products
-    .filter(product => product.handle === 'short-sleeve-t-shirt') // filter for only the t-shirts
-    .flatMap(product => product.variants.map(variant => [variant, variant.image.src, variant.price]));
+    /* Create an aray of unique products from the collecion*/
+    // const itemExists = new Set();
+    // const uniqueProducts = products.filter(el => {
+    //     const duplicate = itemExists.has(el.variants.image);
+    //
+    //     itemExists.add(el.variants.image);
+    //     return !duplicate;
+    // });
 
+    console.log(products);
 
-  const itemExists = new Set();
-  const uniqueProducts = collection.filter(el => {
-    const duplicate = itemExists.has(el[1]);
+    const collection = products.map((product, index) => {
+        const variants = product.variants.map((variant, index) => {
+            if (index % 4 === 0) return variant;
+        });
 
-    itemExists.add(el[1]);
-    return !duplicate;
-  });
+        const title = product.title;
 
-  console.log(uniqueProducts);
+        // console.log(title);
+        // console.log(product.options[0].values);
 
-  const productCard = uniqueProducts.map((product, index) => {
-    const imageUrl = product[1];
-    const productPrice = product[2];
+        // const colorOptions = product.options[0].name;
+
+        const colorOptions = product.options.map((option,index) => {
+            if (option.name === 'Color') {
+                console.log(option)
+            }
+        })
+
+        console.log(colorOptions)
+
+        return (
+            <div className="product_card column" key={index}>
+                <div className="product">
+                    <div className="img_container">
+                        <img src={variants[0].image.src} alt="" className="product_img"/>
+                    </div>
+                    <div className="details_container column">
+                        <h3 className="product_title">{title}</h3>
+                        <p className="price">{variants[0].price}</p>
+                        <div className="color_container row">
+                            <span className="color"></span>
+                            <span className="color"></span>
+                            <span className="color"></span>
+                            <span className="color"></span>
+                            <span className="color"></span>
+                            <span className="color"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    });
+
     return (
-       <div className="product_container column">
-          <div className="image_container">
-            <img className="product_image" src={imageUrl} alt="T-shirt" />
-          </div>
-          <div className="details_container column">
-            <h3 className="product_title">Short sleeve T-shirt</h3>
-            <p className="price_copy"><span>${productPrice}</span></p>
-          <div className="swatch_container row">
-            <span className="color red"></span>
-            <span className="color navy"></span>
-            <span className="color teal"></span>
-            <span className="color grey"></span>
-            <span className="color yellow"></span>
-            <span className="color brown"></span>
-              {/* <div className="swatch_item">red</div>
-              <div className="swatch_item">blue</div>
-              <div className="swatch_item">yellow</div> */}
-            </div>
-            </div>
+        <div className="Collection">
+            {collection}
         </div>
-      )
-   })
-
-  return (
-    <div className="Collection">
-      <div className="wrapper row">
-       {productCard}
-    </div>
-    </div>
-  );
+    );
 }
 
 export default Collection;
